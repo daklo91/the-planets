7768cc02c9d26b1e963cfd4c9b86fc47fed8e64b
daklo91 <coffeedog91@gmail.com>
2024-07-09 11:16:19 +0200

    Add typography and colors from design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,26 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontFamily: {
+      "spartan-regular": ["var(--spartan-regular), sans-serif"],
+      "spartan-bold": ["var(--spartan-bold), sans-serif"],
+      "antonio-medium": ["var(--antonio-medium), sans-serif"],
+    },
+    colors: {
+      primary: "#FFFFFF",
+      secondary: "#838391",
+      accent: "#38384F",
+      background: "#070724",
+      hover: "#313148",
+      mercury: "#419EBB",
+      venus: "#EDA249",
+      earth: "#6F2ED6",
+      mars: "#D14C32",
+      jupiter: "#D83A34",
+      saturn: "#CD5120",
+      uranus: "#1EC2A4",
+      neptune: "#2D68F0",
     },
   },
-  plugins: [],
 };
 export default config;
