138ad9d635f237c75236e5b7672f448f06ceb338
daklo91 <coffeedog91@gmail.com>
2024-07-12 16:14:04 +0200

    Add sidemenu when hamburger is pressed

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,68 +1,184 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
+import Chevron from "../../../assets/icon-chevron.svg";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <header className="flex items-center justify-between border-b-accent border-b pl-[24px] pr-[24px] lg:pl-[32px] lg:pr-[40px] lg:flex-row md:flex-col">
-      <div className="font-antonio-medium text-primary text-[1.75rem] lead-[-1.05px] my-[15px] md:my-[30px] lg:my-0 md:mb-[10px] lg:mb-0">
-        THE PLANETS
-      </div>
-      <nav className="hidden font-spartan-bold text-primary/75 text-[0.875rem] tracking-[0.063rem] md:flex">
+    <>
+      <header className="flex items-center justify-between border-b-accent border-b pl-[24px] pr-[24px] lg:pl-[32px] lg:pr-[40px] lg:flex-row md:flex-col">
+        <div className="font-antonio-medium text-primary text-[1.75rem] lead-[-1.05px] my-[15px] md:my-[30px] lg:my-0 md:mb-[10px] lg:mb-0">
+          THE PLANETS
+        </div>
+        <nav className="hidden font-spartan-bold text-primary/75 text-[0.875rem] tracking-[0.063rem] md:flex">
+          <Link
+            href="/"
+            className="block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-mercury/0 hover:border-mercury focus-visible:border-mercury"
+          >
+            MERCURY
+          </Link>
+          <Link
+            href="/venus"
+            className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-venus/0 hover:border-venus focus-visible:border-venus"
+          >
+            VENUS
+          </Link>
+          <Link
+            href="/earth"
+            className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-earth/0 hover:border-earth focus-visible:border-earth"
+          >
+            EARTH
+          </Link>
+          <Link
+            href="/mars"
+            className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-mars/0 hover:border-mars focus-visible:border-mars"
+          >
+            MARS
+          </Link>
+          <Link
+            href="/jupiter"
+            className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-jupiter/0 hover:border-jupiter focus-visible:border-jupiter"
+          >
+            JUPITER
+          </Link>
+          <Link
+            href="/saturn"
+            className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-saturn/0 hover:border-saturn focus-visible:border-saturn"
+          >
+            SATURN
+          </Link>
+          <Link
+            href="/uranus"
+            className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-uranus/0 hover:border-uranus focus-visible:border-uranus"
+          >
+            URANUS
+          </Link>
+          <Link
+            href="/neptune"
+            className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-neptune/0 hover:border-neptune focus-visible:border-neptune"
+          >
+            NEPTUNE
+          </Link>
+        </nav>
+        <button
+          onClick={() => setMenuOpen((menuOpen) => !menuOpen)}
+          className="w-[24px] block md:hidden"
+        >
+          <div
+            className={`w-full h-[3px]  ${
+              menuOpen ? "bg-accent" : "bg-primary"
+            }`}
+          ></div>
+          <div
+            className={`my-[4px] w-full h-[3px]  ${
+              menuOpen ? "bg-accent" : "bg-primary"
+            }`}
+          ></div>
+          <div
+            className={`w-full h-[3px]  ${
+              menuOpen ? "bg-accent" : "bg-primary"
+            }`}
+          ></div>
+        </button>
+      </header>
+      <nav
+        style={{ left: menuOpen ? "0%" : "100%" }}
+        className="md:hidden overflow-y-auto pt-[24px] pb-[120px] px-[24px] fixed bg-background w-full h-full transition-[left] leading-[0.875rem] font-spartan-bold text-primary text-[0.875rem] tracking-[0.063rem]"
+      >
         <Link
+          onClick={() => setMenuOpen((menuOpen) => !menuOpen)}
           href="/"
-          className="block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-mercury/0 hover:border-mercury focus-visible:border-mercury"
+          className="flex justify-between items-center border-b-accent border-b py-[20px]"
         >
-          MERCURY
+          <div className="flex gap-[25px] items-center">
+            <div className="w-[20px] h-[20px] rounded-full bg-[#DEF4FC]"></div>
+            <div className="mt-[3px]">MERCURY</div>
+          </div>
+          <Image src={Chevron} alt="" className="w-[4px] h-[8px] mr-[4px]" />
         </Link>
         <Link
+          onClick={() => setMenuOpen((menuOpen) => !menuOpen)}
           href="/venus"
-          className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-venus/0 hover:border-venus focus-visible:border-venus"
+          className="flex justify-between items-center border-b-accent border-b py-[20px]"
         >
-          VENUS
+          <div className="flex gap-[25px] items-center">
+            <div className="w-[20px] h-[20px] rounded-full bg-[#F7CC7F]"></div>
+            <div className="mt-[3px]">VENUS</div>
+          </div>
+          <Image src={Chevron} alt="" className="w-[4px] h-[8px] mr-[4px]" />
         </Link>
         <Link
+          onClick={() => setMenuOpen((menuOpen) => !menuOpen)}
           href="/earth"
-          className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-earth/0 hover:border-earth focus-visible:border-earth"
+          className="flex justify-between items-center border-b-accent border-b py-[20px]"
         >
-          EARTH
+          <div className="flex gap-[25px] items-center">
+            <div className="w-[20px] h-[20px] rounded-full bg-[#545BFE]"></div>
+            <div className="mt-[3px]">EARTH</div>
+          </div>
+          <Image src={Chevron} alt="" className="w-[4px] h-[8px] mr-[4px]" />
         </Link>
         <Link
+          onClick={() => setMenuOpen((menuOpen) => !menuOpen)}
           href="/mars"
-          className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-mars/0 hover:border-mars focus-visible:border-mars"
+          className="flex justify-between items-center border-b-accent border-b py-[20px]"
         >
-          MARS
+          <div className="flex gap-[25px] items-center">
+            <div className="w-[20px] h-[20px] rounded-full bg-[#FF6A45]"></div>
+            <div className="mt-[3px]">MARS</div>
+          </div>
+          <Image src={Chevron} alt="" className="w-[4px] h-[8px] mr-[4px]" />
         </Link>
         <Link
+          onClick={() => setMenuOpen((menuOpen) => !menuOpen)}
           href="/jupiter"
-          className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-jupiter/0 hover:border-jupiter focus-visible:border-jupiter"
+          className="flex justify-between items-center border-b-accent border-b py-[20px]"
         >
-          JUPITER
+          <div className="flex gap-[25px] items-center">
+            <div className="w-[20px] h-[20px] rounded-full bg-[#ECAD7A]"></div>
+            <div className="mt-[3px]">JUPITER</div>
+          </div>
+          <Image src={Chevron} alt="" className="w-[4px] h-[8px] mr-[4px]" />
         </Link>
         <Link
+          onClick={() => setMenuOpen((menuOpen) => !menuOpen)}
           href="/saturn"
-          className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-saturn/0 hover:border-saturn focus-visible:border-saturn"
+          className="flex justify-between items-center border-b-accent border-b py-[20px]"
         >
-          SATURN
+          <div className="flex gap-[25px] items-center">
+            <div className="w-[20px] h-[20px] rounded-full bg-[#FCCB6B]"></div>
+            <div className="mt-[3px]">SATURN</div>
+          </div>
+          <Image src={Chevron} alt="" className="w-[4px] h-[8px] mr-[4px]" />
         </Link>
         <Link
+          onClick={() => setMenuOpen((menuOpen) => !menuOpen)}
           href="/uranus"
-          className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-uranus/0 hover:border-uranus focus-visible:border-uranus"
+          className="flex justify-between items-center border-b-accent border-b py-[20px]"
         >
-          URANUS
+          <div className="flex gap-[25px] items-center">
+            <div className="w-[20px] h-[20px] rounded-full bg-[#65F0D5]"></div>
+            <div className="mt-[3px]">URANUS</div>
+          </div>
+          <Image src={Chevron} alt="" className="w-[4px] h-[8px] mr-[4px]" />
         </Link>
         <Link
+          onClick={() => setMenuOpen((menuOpen) => !menuOpen)}
           href="/neptune"
-          className="ml-[33px] block pt-[25px] pb-[20px] lg:pb-[25px] hover:text-primary focus-visible:text-primary border-b-4 lg:border-b-0 lg:border-t-4 border-neptune/0 hover:border-neptune focus-visible:border-neptune"
+          className="flex justify-between items-center border-b-accent border-b py-[20px]"
         >
-          NEPTUNE
+          <div className="flex gap-[25px] items-center">
+            <div className="w-[20px] h-[20px] rounded-full bg-[#497EFA]"></div>
+            <div className="mt-[3px]">NEPTUNE</div>
+          </div>
+          <Image src={Chevron} alt="" className="w-[4px] h-[8px] mr-[4px]" />
         </Link>
       </nav>
-      <button className="w-[24px] block md:hidden">
-        <div className="w-full h-[3px] bg-primary"></div>
-        <div className="w-full h-[3px] my-[4px] bg-primary"></div>
-        <div className="w-full h-[3px] bg-primary"></div>
-      </button>
-    </header>
+    </>
   );
 }
 
--- src/app/components/header/readme.md
+++ src/app/components/header/readme.md
@@ -1,10 +1,20 @@
 # Contains
 
+Two parts, one is the header and the other is the side-menu.
+
+## Header
+
 - Logo
 - Nav Links
 - Hamburger Button
+
+## Sidemenu
+
+- Nav links
+- Uses the state menuOpen (boolean) to toggle the sidemenu. This is used on the hamburger menu button and on every link to toggle it off when navigating.
 
 # Further improvements
 
 - [ ] Make the logo an actual logo (SVG or similar instead of text)
 - [ ] Make the nav links rendered via a list instead of hardcoding (e.g from an array [Mercury, Venus, Earth ...])
+- [ ] Make the menuOpen a toggle function instead of an inline function (makes it more clean)
