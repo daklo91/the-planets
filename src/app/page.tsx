6f79f0a175c7f146ecdbc23e293d906c359dd6c8
daklo91 <coffeedog91@gmail.com>
2024-07-13 17:46:13 +0200

    Update data with image links and colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import planetData from "@/data/data.json";
+import Image from "next/image";
 
 export default function Mercury() {
   return (
     <main>
       <p className="heading-h2">{planetData[0].overview.content}</p>
+      <Image src={planetData[0].images.geology} height={100} width={100} />
     </main>
   );
 }
