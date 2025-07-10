e4b744a1c1cb3888b83126b4d6f7e81d03414892
Dennis Bjørneset <dennis.bjorneset@bas.no>
2025-07-10 18:35:58 +0200

    transfer code to desktop

--- src/app/[planet]/page.tsx
+++ src/app/[planet]/page.tsx
@@ -1,5 +1,6 @@
 import planetDataArray from "@/data/data.json";
 import { notFound } from "next/navigation";
+import iconSource from "";
 
 interface PlanetPageProps {
   params: {
@@ -19,7 +20,28 @@ export default function PlanetPage({ params }: PlanetPageProps) {
 
   return (
     <main>
-      <p className="heading-h2">{PLANET_DATA.name}</p>
+      <h1 className="heading-h1">{PLANET_DATA.name}</h1>
+      <p className="text-primary font-spartan-regular">
+        {PLANET_DATA.overview.content}
+      </p>
+
+      <img src={PLANET_DATA.images.planet} />
+
+      <p className="text-secondary">
+        Source :{" "}
+        <a
+          href={PLANET_DATA.overview.source}
+          target="_blank"
+          className="font-bold underline"
+        >
+          Wikipedia
+          <img src="/images/icon-source.svg" className="inline-block ml-2" />
+        </a>
+      </p>
+
+      <button className="text-primary py-3 border-accent border-solid border">
+        <span className="mx-6">01</span> OVERVIEW
+      </button>
     </main>
   );
 }
