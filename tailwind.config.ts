2eb1119bc8203c72796d37045880f2c4eac5b14d
daklo91 <coffeedog91@gmail.com>
2025-08-08 11:33:27 +0200

    Update classes in tilwind to be important

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   safelist: [
-    ...Object.keys(planetColors).map(color => `bg-${color}`)
+    ...Object.keys(planetColors).map(color => `!bg-${color}`)
   ],
   theme: {
     fontFamily: {
