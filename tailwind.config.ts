5fa3a9d2589f491e91ba935cc9b6a001ab603873
daklo91 <coffeedog91@gmail.com>
2025-09-18 10:25:36 +0200

    Remove hardcoded planetColors from tailwind safelist and colors The color now comes from the data

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,10 @@
 import type { Config } from "tailwindcss";
 
-const planetColors = {
-  mercury: "#419EBB",
-  venus: "#EDA249",
-  earth: "#6F2ED6",
-  mars: "#D14C32",
-  jupiter: "#D83A34",
-  saturn: "#CD5120",
-  uranus: "#1EC2A4",
-  neptune: "#2D68F0",
-}
-
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  safelist: [
-    ...Object.keys(planetColors).map(color => `!bg-${color}`)
   ],
   theme: {
     fontFamily: {
@@ -31,7 +17,6 @@ const config: Config = {
       accent: "#38384F",
       background: "#070724",
       hover: "#313148",
-      ...planetColors
     },
   }
 };
